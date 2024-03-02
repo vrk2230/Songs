f226c9025bbc7efb6c0caaec058b6f3453882788
vrk2230 <ram.koushik17@gmail.com>
2024-03-02 19:37:24 +0000

    Main

---
+++ src/main/java/com/example/song/controller/SongController.java
@@ -0,0 +1,46 @@
+package com.example.song.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.*;
+import com.example.song.model.Song;
+import com.example.song.service.SongH2Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+public class SongController {
+
+    @Autowired
+    private SongH2Service songService;
+
+    @GetMapping("/songs")
+    public List<Song> getSongs() {
+        return songService.getSongs();
+    }
+
+    @PostMapping("/songs")
+    public Song addSong(@RequestBody Song song) {
+        return songService.addSong(song);
+    }
+
+    @GetMapping("/songs/{songId}")
+    public Song getSongById(@PathVariable("songId") int songId) {
+        return songService.getSongById(songId);
+    }
+
+    @DeleteMapping("/songs/{songId}")
+    public void deleteSongById(@PathVariable("songId") int songId) {
+        songService.deleteSong(songId);
+    }
+
+    @PutMapping("/songs/{songId}")
+    public Song updateSong(@PathVariable("songId") int songId, @RequestBody Song song) {
+        return songService.updateSong(songId, song);
+    }
+
+}
